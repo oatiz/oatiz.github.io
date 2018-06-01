bfc74303494a6775f0d729d004a7cda25959bc63
oatiz <2278966200@qq.com>
2018-06-02 01:59:55 +0800

    remove appveyor

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -15,7 +15,7 @@ git add .
 git commit -m "Site updated: `date +"%Y-%m-%d %H:%M:%S"`"
 
 # 同时 push 一份到自己的服务器上
-git remote add vps git@prinzeugen.net:hexo.git
+# git remote add vps git@prinzeugen.net:hexo.git
 
-git push vps master:master --force --quiet
-git push origin master:master --force --quiet
+# git push vps master:master --force --quiet
+# git push origin master:master --force --quiet
